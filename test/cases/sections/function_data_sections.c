17dae128e2da2c9d6d47643f31a47a3a87c64344
Rob Pilling <robpilling@gmail.com>
2021-01-05 22:44:43 +0000

    WIP: Check d->bits.var.init - may be null for <SOME REASON>

--- test/cases/sections/function_data_sections.c
+++ test/cases/sections/function_data_sections.c
@@ -6,16 +6,22 @@
 // RUN: grep -F '.section .data.j' %t
 // RUN: grep -F '.section .data.k' %t
 
+// FIXME: asmcheck, /^f:/ .. /^Lfuncend_f/
+//                        ^~ CHECK-NOT: /\.section/
+// RUN: %layout_check
+
 i = 3, j = 4, k = 5;
 
-f()
+void f()
 {
 }
 
-g()
+void g(int a)
 {
+	if(a)
+		f();
 }
 
-main()
+int main()
 {
 }
