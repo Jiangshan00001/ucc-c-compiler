6a2c66bbcde464a347caeb6d1a65aabaa9279f5b
Rob Pilling <robpilling@gmail.com>
2021-01-05 23:19:55 +0000

    Improve passability checks, especially switch-with-all-cases-returning

--- src/cc1/ops/stmt_code.c
+++ src/cc1/ops/stmt_code.c
@@ -620,7 +620,7 @@ void style_stmt_code(const stmt *s, out_ctx *octx)
 	stylef("\n}\n");
 }
 
-static int code_passable(stmt *s)
+static int code_passable(stmt *s, int break_means_passable)
 {
 	stmt **i;
 
@@ -628,7 +628,7 @@ static int code_passable(stmt *s)
 
 	for(i = s->bits.code.stmts; i && *i; i++){
 		stmt *sub = *i;
-		if(!fold_passable(sub))
+		if(!fold_passable(sub, break_means_passable))
 			return 0;
 	}
 
