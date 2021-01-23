ce5941d3c0ba88eded00b3b11d1bd29568f9897a
Rob Pilling <robpilling@gmail.com>
2021-01-23 18:25:01 +0000

    Debugging

--- test/cases/vla/vm_init.c
+++ test/cases/vla/vm_init.c
@@ -1,5 +1,8 @@
-// RUN: %ocheck 0 %s
-void abort(void) __attribute__((noreturn));
+// RUN: %ucc -c -o %t %s -w
+// RUN: objdump -d %t || true
+// RUN: %ocheck 0 %s -w
+
+void exit(int) __attribute__((noreturn));
 
 syntax(int n)
 {
@@ -9,18 +12,29 @@ syntax(int n)
 	syntax(p);
 }
 
-assert(_Bool b)
+assert(_Bool b, int lno)
 {
-	if(!b)
-		abort();
+	if(!b){
+		printf("%s:%d: assert failed\n", __FILE__, lno);
+		exit(1);
+	}
 }
+#define assert(x) assert((x), __LINE__)
 
 f(int n)
 {
 	short (*p)[n] = 0;
 
+	printf("p = %#x\n", p);
 	assert(p == 0);
-	assert((int)(p + 1) == 3 * sizeof(short));
+
+	__auto_type a = (int)(p + 1);
+	__auto_type b = 3 * sizeof(short);
+	if(a != b){
+		printf("a=%#x\nb=%#x\n", a, b);
+		fflush(0);
+		exit(5);
+	}
 }
 
 main()
